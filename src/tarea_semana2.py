3fb7c0556951bb350d3b1f9966348787b2fc1731
Luis Flores <lsl.fflores.001@gmail.com>
2025-12-21 11:19:47 -0500

    update

--- src/tarea_semana2.py
+++ src/tarea_semana2.py
@@ -3,6 +3,7 @@ MODE2="UMBRAL_MEDIO"
 MODE3="UMBRAL_ALTO"
 STUDENT="lUIS"
 accumulate=0                #Declarar variable como número
+list=[]                     # Variable como lista que almacena valores de voltaje o tempertura 
 
 str1=input("Enter your name ")
 str2=input("Enter the name of the electronic device ")
@@ -10,12 +11,14 @@ num_samples=int(input("Enter the number of samples "))
 user_choice=int(input("choose an option 1- VOLTAGE OR 2- TEMPERATURE "))
 
 try:
+    # logica condicional segun la leccion del usuario 1-2
     if user_choice==1:
+        
+        
         for i in range(0,2):
             voltage=float(input(f"Enter the first voltage value {i+1}: " ))
-            accumulate=accumulate + voltage
-
-
+            list.append(voltage)
+            accumulate=accumulate + voltage # variable que acumula valores de volataje
         if accumulate<10:
             print(f"Stutus: {MODE1}")
         elif accumulate<20:
@@ -24,13 +27,16 @@ try:
             print(f"Stutus: {MODE3}")
 
 
-        print(f"student : {STUDENT} | Device: {str2}" )   
-        print(f"Readings: {voltage} ") 
+        print(f"student : {STUDENT} | Device: {str2}" ) 
+        # Bulce que imprime los valores almacenados en la lista 
+        for i in list:
+             print(f"Voltaje reading: {i}")
         print(f"Average: {(accumulate/2):.2f}")
     else:
         for i in range(0,2):
             temperature=float(input(f"Enter the first temperature value {i+1}: " ))
-            accumulate=accumulate + temperature
+        
+            accumulate=accumulate + temperature # variable que acumula valores de temperatura
 
         if accumulate<10:
             print(f"Stutus: {MODE1}")
@@ -40,11 +46,16 @@ try:
             print(f"Stutus: {MODE3}")
 
 
-        print(f"student : {STUDENT} | Device: {str2}" )   
+        print(f"student : {STUDENT} | Device: {str2}" ) 
+        # Bulce que imprime los valores almacenados en la lista 
+        for i in list:
+            print(f"Temperature reading: {i}")
+
         print(f"Readings: {temperature} ") 
         print(f"Average: {(accumulate/2):.2f}")
 
 except ValueError:
+    # Salida si la variable user_choice no es un numero entero
     print("Invalid input. Choose an option from the menu(e.g. 1 or 2)")
 
 
